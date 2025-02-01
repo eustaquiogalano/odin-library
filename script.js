a5d151620e996a464af5e94aa42019700392db67
eustaquio <eustaquio.galano@gmail.com>
2025-02-01 09:25:01 +0800

    feat: add basic skeleton guide from Odin

---
+++ script.js
@@ -0,0 +1,12 @@
+const myLibrary = [];  // our main storage for library
+
+function Book(title, author, numberOfPages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary() {
+    
+}
