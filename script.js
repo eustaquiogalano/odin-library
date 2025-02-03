a893a17ad23f388ef2214201012561fca2053723
eustaquio <eustaquio.galano@gmail.com>
2025-02-03 15:40:55 +0800

    feat: Add modal button function
    - The "add to list" button inside the modal will gather data from
    the input elements inside the dialog element
    - gathered data will be put inside the newly created book object
    - The new book object created will be added into the temporary storage
    named the "myLibrary" array.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const addBookButton = document.querySelector("#add-book");
 const addBookModal = document.querySelector("#add-book-modal");
+const addToListButton = document.querySelector("#add-to-list");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
 
 const myLibrary = [];  // our main storage for library
 
@@ -10,11 +14,25 @@ function Book(title, author, numberOfPages, isRead) {
     this.isRead = isRead;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(newBook) {
+    myLibrary.push(newBook);
+    console.log( myLibrary );
     
 }
 
 addBookButton.addEventListener('click', () => {
     addBookModal.showModal();
+});
+
+addToListButton.addEventListener("click", () => {
+    console.log(title.value, author.value, pages.value , "here");
+
+    let bookTitle = title.value;
+    bookTitle = new Book(bookTitle, author.value, pages.value);
+    
+    addBookToLibrary(bookTitle);
+    title.value = "";
+    author.value = "";
+    pages.value = "";
 });
 
