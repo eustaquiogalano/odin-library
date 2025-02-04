35dc866b3ef2e00ee2113cf7ff71d6f2be8672a6
eustaquio <eustaquio.galano@gmail.com>
2025-02-04 18:59:21 +0800

    feat: Add delete function on the button

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const addToListButton = document.querySelector("#add-to-list");
 const title = document.querySelector("#title");
 const author = document.querySelector("#author");
 const pages = document.querySelector("#pages");
-const bookList = document.querySelector("#book-section"); 
+const bookList = document.querySelector("#book-section");
 
 const myLibrary = [
     {
@@ -46,12 +46,12 @@ function renderBookList() {
     let generatedHTML = "";
     
     // loop over the array
-    myLibrary.forEach(book => {
+    myLibrary.forEach((book, index) => {
 
         // Get the book title and remove whitespaces
         // to use as id name for each books input and label elements  
         let idName = book.title.split(" ").join("");
-        
+
         // This will generate a container for each book
         // including all the elements and attributes needed 
         generatedHTML += 
@@ -71,8 +71,8 @@ function renderBookList() {
                         <label for="${idName}" class="js-manipulation"></label>
                     </div>
                 </div>
-                <div class="delete-${idName} del">
-                    <button>Delete</button>
+                <div class="del">
+                    <button class="delete-button" id="${index}"}">Delete</button>
                 </div>
             </div>
             `;
@@ -81,6 +81,34 @@ function renderBookList() {
     // The generated containers will be the content 
     // of the book list section 
     bookList.innerHTML = generatedHTML;
+
+    // this function will put an event listener 
+    // to every delete button of every book container
+    deleteBook();
+}
+
+function deleteBook() {
+    // gather all the current delete button 
+    const deleteBookButton = document.querySelectorAll(".delete-button"); 
+
+    // loop over the node list of delete button
+    deleteBookButton.forEach(button => {
+
+        // put an event listener to every delete button
+        button.addEventListener("click", (e) => {
+
+            // get the ID of the clicked element
+            // convert to number to be able to use as a index number
+            let bookIndex = +e.target.id;
+
+            // remove the selected element by referencing
+            // its index number
+            myLibrary.splice(bookIndex, 1);
+
+            // render the array
+            renderBookList();
+        });
+    })
 }
 
 addBookButton.addEventListener('click', () => {
